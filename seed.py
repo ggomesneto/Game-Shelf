074f5f1d3116d6b40112200f05b7b5a2bd4c322f
ggomesneto <69973423+ggomesneto@users.noreply.github.com>
2020-11-23 09:09:54 -0600

    Update seed.py

--- seed.py
+++ seed.py
@@ -4,26 +4,45 @@ from models import db, Review, User, Collection
 db.drop_all()
 db.create_all()
 
-user1 = User(
-    username='ggomesneto',
-    first_name='Geraldo',
-    last_name='Gomes',
-    email='email@email.com'
-    
-)
+name='JohnLennon',
+pwd='123'
+first_name='John',
+last_name='Lennon',
+
+user1 = User.register(name,pwd,first_name, last_name)
+
+name='RingoStar',
+pwd='123'
+first_name='Ringo',
+last_name='Star',
+
+user2 = User.register(name,pwd,first_name, last_name)
 
 collection1 = Collection(
-    username='ggomesneto',
+    username='JohnLennon',
     game_slug='the-witcher-3-wild-hunt'
 )
 
+collection2 = Collection(
+    username='RingoStar',
+    game_slug='fall-guys'
+)
+
 review1 = Review(
-    username='ggomesneto',
+    username='JohnLennon',
     game_name='The Witcher 3 Wild Hunt',
     game_slug='the-witcher-3-wild-hunt',
     review='AWESOME GAME',
     title='I LOVE THIS GAME'
 )
 
-db.session.add_all([user1,collection1,review1])
+review2 = Review(
+    username='RingoStar',
+    game_name='Cyberpunk 2077',
+    game_slug='cyberpunk-2077',
+    review='Wow',
+    title='Nyce'
+)
+
+db.session.add_all([user1, user2,collection1, collection2,review1,review2])
 db.session.commit()
